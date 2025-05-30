887d801633faf4ebb65dccf7c37994c36bb6c2f0
mayur <khaniymayur920@gmail.com>
2025-05-30 08:25:48 +0530

    sinup and sinin feature added

--- opps_proj.py
+++ opps_proj.py
@@ -16,10 +16,10 @@ class chatbook:
                            5 press 5 to other key to exit""")
         
         if user_input == "1":
-            pass
+            self.sigup()
 
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
             pass
         elif user_input == "4":
@@ -27,5 +27,28 @@ class chatbook:
         else : 
             exit()
 
+    def sigup(self):
+        email = input("Enter your email here -> ")
+        passwordd = input("Enter your password here -> ")
+        self.username = email
+        self.password = passwordd                
+        print("you heve signed up successfully !!")
+        print("\n")
+        self.menu()
 
+    def signin(self):
+        if self.username == "" and self.password == "":
+            print("please sinup first by pressing 1 in the mail menu")
+        else:
+            uname = input("Enter your email/username here -> ")
+            pws = input("Enter your password here -> ")
+            if self.username == uname and self.password == pws:
+                print("you have signed in successfully !!")
+                self.loggedin = True
+            else:
+                print("please input correct creadintials..")
+
+        print("\n")
+        self.menu()
+# object create
 obj = chatbook()
